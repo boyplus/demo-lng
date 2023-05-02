affbb8dd89e8f94ecfc5f4d00ec8fd28bba73d40
boyplus <ict224bj@gmail.com>
2023-05-02 13:15:12 +0700

    update: API

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService, CountResult } from './app.service';
 
 @Controller()
 export class AppController {
@@ -8,5 +8,21 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('count')
+  async getCount(): Promise<CountResult> {
+    const count = await this.appService.getCount();
+    return count;
+  }
+
+  @Post('add-count')
+  async addCountByOne(): Promise<void> {
+    await this.appService.addCountByOne();
+  }
+
+  @Post('substract-count')
+  async substractCountByOne(): Promise<void> {
+    await this.appService.substractCountByOne();
   }
 }
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Client } from 'pg';
+
+export interface CountResult {
+  count: number;
+}
 
 @Injectable()
 export class AppService {
+  DB_URL = '';
+
+  constructor(private configService: ConfigService) {
+    this.DB_URL = this.configService.get<string>('DATABASE_URL');
+  }
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getCount(): Promise<CountResult> {
+    const client = new Client(this.DB_URL);
+    try {
+      await client.connect();
+      const results = await client.query(
+        'select num from defaultdb.count_test',
+      );
+      const count = parseInt(results.rows[0].num);
+      return { count };
+    } catch (error) {
+    } finally {
+      client.end();
+    }
+    return { count: 0 };
+  }
+
+  async addCountByOne(): Promise<void> {
+    const client = new Client(this.DB_URL);
+    try {
+      await client.connect();
+      await client.query('update count_test set num = num+1 where id = 1;');
+    } catch (error) {
+    } finally {
+      client.end();
+    }
+  }
+
+  async substractCountByOne(): Promise<void> {
+    const client = new Client(this.DB_URL);
+    try {
+      await client.connect();
+      await client.query('update count_test set num = num-1 where id = 1;');
+    } catch (error) {
+    } finally {
+      client.end();
+    }
   }
 }
