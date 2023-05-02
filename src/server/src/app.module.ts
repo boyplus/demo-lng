145ded4f7dc1d2810b0079fdfd0ceae043076c31
boyplus <ict224bj@gmail.com>
2023-05-02 18:36:53 +0700

    move: folder

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'wwwroot'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
