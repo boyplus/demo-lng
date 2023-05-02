ecf839a7f75f879b58eb91c66c5230809c698d19
boyplus <ict224bj@gmail.com>
2023-05-02 19:23:31 +0700

    update

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -38,7 +38,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Hello
         </p>
       </div>
       <p className="read-the-docs">
